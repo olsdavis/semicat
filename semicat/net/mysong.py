0287b98f6cc99fc2955bbca0cc78897d1182657b
olsdavis <olsdavis@gmail.com>
2026-01-14 16:44:18 +0000

    code free of auxiliary BS

--- semicat/net/mysong.py
+++ semicat/net/mysong.py
@@ -12,6 +12,7 @@ import math
 import numpy as np
 import torch
 from torch import nn
+from torch.nn import functional as F
 from torch.nn.functional import silu
 
 
@@ -37,104 +38,6 @@ def weight_init(shape, mode, fan_in, fan_out):
     if mode == "kaiming_normal":
         return np.sqrt(1 / fan_in) * torch.randn(*shape)
     raise ValueError(f'Invalid init mode "{mode}"')
-
-# ----------------------------------------------------------------------------
-# Convolutional layer with optional up/downsampling.
-
-class Conv1d(torch.nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel,
-        bias=True,
-        up=False,
-        down=False,
-        resample_filter=[1, 1],
-        fused_resample=False,
-        init_mode="kaiming_normal",
-        init_weight=1,
-        init_bias=0,
-    ):
-        assert not (up and down)
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.up = up
-        self.down = down
-        self.fused_resample = fused_resample
-        init_kwargs = dict(
-            mode=init_mode,
-            fan_in=in_channels * kernel * kernel,
-            fan_out=out_channels * kernel * kernel,
-        )
-        self.weight = (
-            torch.nn.Parameter(
-                weight_init([out_channels, in_channels, kernel], **init_kwargs)
-                * init_weight
-            )
-            if kernel
-            else None
-        )
-        self.bias = (
-            torch.nn.Parameter(weight_init([out_channels], **init_kwargs) * init_bias)
-            if kernel and bias
-            else None
-        )
-        f = torch.as_tensor(resample_filter, dtype=torch.float32)
-        f = f.unsqueeze(0) / f.sum().square()
-        self.register_buffer("resample_filter", f if up or down else None)
-
-    def forward(self, x):
-        w = self.weight if self.weight is not None else None
-        b = self.bias if self.bias is not None else None
-        f = (
-            self.resample_filter
-            if self.resample_filter is not None
-            else None
-        )
-        w_pad = w.shape[-1] // 2 if w is not None else 0
-        f_pad = (f.shape[-1] - 1) // 2 if f is not None else 0
-
-        if self.fused_resample and self.up and w is not None:
-            x = torch.nn.functional.conv_transpose1d(
-                x,
-                f.mul(4).tile([self.in_channels, 1, 1]),
-                groups=self.in_channels,
-                stride=2,
-                padding=max(f_pad - w_pad, 0),
-            )
-            x = torch.nn.functional.conv1d(x, w, padding=max(w_pad - f_pad, 0))
-        elif self.fused_resample and self.down and w is not None:
-            x = torch.nn.functional.conv1d(x, w, padding=w_pad + f_pad)
-            x = torch.nn.functional.conv1d(
-                x,
-                f.tile([self.out_channels, 1, 1]),
-                groups=self.out_channels,
-                stride=2,
-            )
-        else:
-            if self.up:
-                x = torch.nn.functional.conv_transpose1d(
-                    x,
-                    f.mul(4).tile([self.in_channels, 1, 1]),
-                    groups=self.in_channels,
-                    stride=2,
-                    padding=f_pad,
-                )
-            if self.down:
-                x = torch.nn.functional.conv1d(
-                    x,
-                    f.tile([self.in_channels, 1, 1]),
-                    groups=self.in_channels,
-                    stride=2,
-                    padding=f_pad,
-                )
-            if w is not None:
-                x = torch.nn.functional.conv1d(x, w, padding=w_pad)
-        if b is not None:
-            x = x.add_(b.reshape(1, -1, 1))
-        return x
 
 
 # ----------------------------------------------------------------------------
@@ -186,47 +89,80 @@ class UNetBlock(torch.nn.Module):
         self.skip_scale = skip_scale
 
         self.norm0 = WrapGroupNorm(num_channels=in_channels, eps=eps)
-        self.conv0 = Conv1d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel=3,
-            up=up,
-            down=down,
-        )
+        if up:
+            self.conv0 = nn.ConvTranspose1d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                output_padding=1,
+            )
+        elif down:
+            self.conv0 = nn.Conv1d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+            )
+        else:
+            self.conv0 = nn.Conv1d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                padding=1,
+            )
         self.affine = nn.Linear(
             in_features=emb_channels,
             out_features=out_channels,
         )
         self.norm1 = WrapGroupNorm(num_channels=out_channels, eps=eps)
-        self.conv1 = Conv1d(
-            in_channels=out_channels, out_channels=out_channels, kernel=3
+        self.conv1 = nn.Conv1d(
+            in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=1
         )
 
         self.skip = None
-        if out_channels != in_channels or up or down:
-            kernel = 1 if resample_proj or out_channels != in_channels else 0
-            self.skip = Conv1d(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                kernel=kernel,
-                up=up,
-                down=down,
-            )
+        if (out_channels != in_channels or up or down) and (resample_proj or out_channels != in_channels):
+            if up:
+                self.skip = nn.ConvTranspose1d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=1,
+                    stride=2,
+                    padding=0,
+                    output_padding=1,
+                )
+            elif down:
+                self.skip = nn.Conv1d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=1,
+                    stride=2,
+                    padding=0,
+                )
+            else:
+                self.skip = nn.Conv1d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=1,
+                    padding=0,
+                )
 
         if self.num_heads:
             self.norm2 = WrapGroupNorm(num_channels=out_channels, eps=eps)
-            self.qkv = Conv1d(
+            self.qkv = nn.Conv1d(
                 in_channels=out_channels,
                 out_channels=out_channels * 3,
-                kernel=1,
+                kernel_size=1,
             )
-            self.proj = Conv1d(
+            self.proj = nn.Conv1d(
                 in_channels=out_channels,
                 out_channels=out_channels,
-                kernel=1,
+                kernel_size=1,
             )
 
-    def forward(self, x, emb):
+    def forward(self, x, emb, jvp_attention: bool = False):
         orig = x
         x = self.conv0(silu(self.norm0(x)))
 
@@ -343,9 +279,7 @@ class MySongUNet(torch.nn.Module):
         # self.enc = torch.nn.ModuleDict()
         self.enc = nn.ModuleList()
         cout = in_channels
-        self.pre_proj = Conv1d(
-            in_channels=cout, out_channels=model_channels, kernel=3,
-        )
+        self.pre_proj = nn.Conv1d(in_channels=cout, out_channels=model_channels, kernel_size=3, padding=1)
 
         for level, mult in enumerate(channel_mult):
             res = img_resolution >> level
@@ -410,7 +344,7 @@ class MySongUNet(torch.nn.Module):
                 continue
 
         self.fin_norm = WrapGroupNorm(num_channels=cout, eps=1e-6)
-        self.fin_conv = Conv1d(in_channels=cout, out_channels=out_channels, kernel=3)
+        self.fin_conv = nn.Conv1d(in_channels=cout, out_channels=out_channels, kernel_size=3, padding=1)
 
     def forward(self, x, s, t, jvp_attention: bool = False):
         t = t.view(-1)
@@ -441,7 +375,7 @@ class MySongUNet(torch.nn.Module):
         skips = [x]
 
         for block in self.enc:
-            x = block(x, emb)
+            x = block(x, emb, jvp_attention=jvp_attention)
             skips.append(x)
 
         # Decoder.
@@ -450,7 +384,7 @@ class MySongUNet(torch.nn.Module):
         for block in self.dec:
             if x.shape[1] != block.in_channels:
                 x = torch.cat([x, skips.pop()], dim=1)
-            x = block(x, emb)
+            x = block(x, emb, jvp_attention=jvp_attention)
 
         tmp = self.fin_norm(x)
         tmp = self.fin_conv(silu(tmp))
